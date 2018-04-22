6dbc4d79dfd96fb93d2bf2179c2f435baacd5062
Celakanth <christian@celakanth.co.za>
2018-04-22 13:19:51 +0100

    Output Clean up

--- src/servo_new.ino
+++ src/servo_new.ino
@@ -352,7 +352,7 @@ void serialCheck()
       Serial.println("In coby actions");
       AllValues = Serial.readString();
       //AllValues = AllValues + ReadingByte;
-      Serial.print(AllValues);
+      Serial.println(AllValues);
       if(AllValues == "exit"){
         AlexaInUse = false;
         Serial.println("Coby is deactive");
