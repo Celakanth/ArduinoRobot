079595438bcbaec89960dfc65a0ef68891271fb7
Celakanth <christian@celakanth.co.za>
2018-04-22 12:59:44 +0100

    Added Responce info

--- src/servo_new.ino
+++ src/servo_new.ino
@@ -455,9 +455,8 @@ void alexaRun(String device, String direction,int degree){
         myServos[7].write(degree);
         
       }
+      Serial.print("I have moved " + device + " " + degree + " degrees");
 }
-
-
 
 // SonarSensor function used to generate and read the ultrasonic wave
 void SonarSensor(int trigPinSensor, int echoPinSensor) //it takes the trigPIN and the echoPIN
